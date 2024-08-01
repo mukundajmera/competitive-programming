7647afe02e414634d4ae2ca1e29c9be6a46f62e8
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-01 23:52:27 +0530

    Time: 733 ms (61.47%), Space: 27.4 MB (64.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #using 2 pointer
         buyAt = prices[0]
         profit = 0
         for price in range(1, len(prices)):
