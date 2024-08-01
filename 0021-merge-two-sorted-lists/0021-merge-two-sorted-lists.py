16bf6039bc69aa5da768a204ebf1c8317b00fd55
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-01 23:44:20 +0530

    Time: 41 ms (37.7%), Space: 16.5 MB (73.93%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        # maintain an unchanging reference to node ahead of the return node.
+        prehead = ListNode(-1)
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            #moving prev pointer for next value
+            prev = prev.next
+
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
