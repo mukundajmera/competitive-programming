0b5a5ae91a0d55285acc78610f4c7b40ab0f4d3f
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-02 23:26:45 +0530

    Time: 60 ms (27.31%), Space: 20.3 MB (67.97%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,23 +7,31 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return
-        lca = None
-        def helper(root, p, q):
-            nonlocal lca
-            if not root:
-                return False
-            isLeft, isRight = False, False
-            if root.left:
-                isLeft = helper(root.left, p, q)
-            if root.right:
-                isRight = helper(root.right, p, q)
-            isCurrent = False
-            if root == p or root == q:
-                isCurrent = True
-            if isCurrent + isLeft + isRight >= 2:
-                lca = root
-            return isCurrent or isLeft or isRight
-        helper(root, p, q)
-        return lca      
+        current_node = root
+        while current_node:
+            if p.val > current_node.val and q.val > current_node.val:
+                current_node = current_node.right
+            elif p.val < current_node.val and q.val < current_node.val:
+                current_node = current_node.left
+            else:
+                return current_node
+        # if not root:
+        #     return
+        # lca = None
+        # def helper(root, p, q):
+        #     nonlocal lca
+        #     if not root:
+        #         return False
+        #     isLeft, isRight = False, False
+        #     if root.left:
+        #         isLeft = helper(root.left, p, q)
+        #     if root.right:
+        #         isRight = helper(root.right, p, q)
+        #     isCurrent = False
+        #     if root == p or root == q:
+        #         isCurrent = True
+        #     if isCurrent + isLeft + isRight >= 2:
+        #         lca = root
+        #     return isCurrent or isLeft or isRight
+        # helper(root, p, q)
+        # return lca      
