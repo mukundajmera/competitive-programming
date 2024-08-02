6161cc9f54beb8913d4c6e2af3673aa88a6aa75a
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-03 03:22:25 +0530

    Time: 46 ms (9.75%), Space: 16.6 MB (53.18%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,23 +1,39 @@
 class Solution:
     def addBinary(self, a, b) -> str:
-        n = max(len(a), len(b))
-        a, b = a.zfill(n), b.zfill(n)
-        
         answer = []
-        count = 0
-        for idx in reversed(range(n)):
-            if a[idx] == "1":
-                count += 1
-            if b[idx] == "1":
-                count += 1
-            if count % 2 == 1:
-                answer.append('1')
-            else:
-                answer.append('0')
-            count = count // 2
-        if count == 1:
-            answer.append('1')
+        carry = 0
+        ida, idb = len(a)-1, len(b)-1
+        while ida >= 0 or idb >= 0 or carry:
+            if ida >= 0:
+                carry += int(a[ida])
+                ida -= 1
+            if idb >= 0:
+                carry += int(b[idb])
+                idb -= 1
+            answer.append(str(carry % 2))
+            carry //= 2
         return "".join(answer[::-1])
+            
+
+    # def addBinary(self, a, b) -> str:
+    #     n = max(len(a), len(b))
+    #     a, b = a.zfill(n), b.zfill(n)
+        
+    #     answer = []
+    #     count = 0
+    #     for idx in reversed(range(n)):
+    #         if a[idx] == "1":
+    #             count += 1
+    #         if b[idx] == "1":
+    #             count += 1
+    #         if count % 2 == 1:
+    #             answer.append('1')
+    #         else:
+    #             answer.append('0')
+    #         count = count // 2
+    #     if count == 1:
+    #         answer.append('1')
+    #     return "".join(answer[::-1])
 
 
     # def addBinary(self, a, b) -> str:
