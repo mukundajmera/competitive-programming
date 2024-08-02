2403d5bba9b7e358a525425e424eefe8275cb8ba
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-03 03:11:39 +0530

    Time: 157 ms (96.8%), Space: 18.1 MB (16.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -9,8 +9,7 @@ class Solution:
             if count == 0:
                 majority = num
                 count = 1
-            else:
-                count -= 1
+            count -= 1
         return majority
     # def majorityElement(self, nums: List[int]) -> int:
     #     numbers = Counter(nums)
