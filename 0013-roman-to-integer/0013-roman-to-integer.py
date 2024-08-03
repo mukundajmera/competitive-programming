a30576f1d3a6760a3d12f383730a6cf757b51457
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-04 00:12:56 +0530

    Time: 44 ms (71.13%), Space: 16.4 MB (83.63%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        rval = {"I":1,
+                "V":5,
+                "X":10,
+                "L":50,
+                "C":100,
+                "D":500,
+                "M":1000}
+        idx = len(s)-1
+        value = 0
+        while idx >= 0:
+            curr_val = rval.get(s[idx])
+            negative = 0
+            while idx-1 >= 0 and rval.get(s[idx - 1]) < curr_val:
+                idx -= 1
+                negative += rval.get(s[idx])
+            value += (curr_val - negative)
+            # print(idx, curr_val, negative, value)
+            idx -= 1
+        return value
