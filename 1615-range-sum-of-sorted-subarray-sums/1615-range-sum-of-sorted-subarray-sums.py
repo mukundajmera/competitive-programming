723e35f40743edf8bfe36694d1862bebaff20461
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-04 23:50:34 +0530

    Time: 227 ms (57.96%), Space: 38 MB (74.78%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
@@ -3,15 +3,12 @@ class Solution:
         store_subarray = []
         for i in range(len(nums)):
             sum = 0
-            # Iterate through all indices ahead of the current index.
             for j in range(i, len(nums)):
                 sum += nums[j]
                 store_subarray.append(sum)
 
-        # Sort all subarray sum values in increasing order.
         store_subarray.sort()
 
-        # Find the sum of all values between left and right.
         range_sum = 0
         mod = 10**9 + 7
         for i in range(left - 1, right):
