20786a615ffa78204484d45ce43a5cb2a62c1da6
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-06 15:17:04 +0530

    Time: 144 ms (81.15%), Space: 17.7 MB (8.11%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,16 +1,34 @@
 import string
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        char_map = [0] * 26
-        for ch in word:
-            char_map[ord(ch) - ord('a')] += 1
-        char_map.sort(reverse=True)
-        # print(char_map)
-        count = 0
-        #total count
-        for idx in range(26):
-            if char_map[idx] == 0:
-                continue
-            count += ((idx // 8) + 1) * char_map[idx]
-            # print(idx, count)
-        return count
+        # Frequency map to store count of each letter
+        frequency_map = Counter(word)
+
+        # Priority queue to store frequencies in descending order
+        frequency_queue = [-freq for freq in frequency_map.values()]
+        heapq.heapify(frequency_queue)
+
+        total_pushes = 0
+        index = 0
+
+        # Calculate total number of presses
+        while frequency_queue:
+            total_pushes += (1 + (index // 8)) * (
+                -heapq.heappop(frequency_queue)
+            )
+            index += 1
+        return total_pushes
+
+        # char_map = [0] * 26
+        # for ch in word:
+        #     char_map[ord(ch) - ord('a')] += 1
+        # char_map.sort(reverse=True)
+        # # print(char_map)
+        # count = 0
+        # #total count
+        # for idx in range(26):
+        #     if char_map[idx] == 0:
+        #         continue
+        #     count += ((idx // 8) + 1) * char_map[idx]
+        #     # print(idx, count)
+        # return count
