a179d74911fe09d0080f4c30e6ebb20f08afd8d7
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-09 21:39:31 +0530

    Time: 582 ms (78.12%), Space: 23 MB (54.64%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,4 +1,20 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points.sort(key = lambda x : x[0] ** 2  + x[1] ** 2)
-        return points[:k]
+        # points.sort(key = lambda x : x[0] ** 2  + x[1] ** 2)
+        # return points[:k]
+
+        heap = [(-self.squared_distance(points[i]), i) for i in range(k)]
+        heapq.heapify(heap)
+        for i in range(k, len(points)):
+            dist = -self.squared_distance(points[i])
+            if dist > heap[0][0]:
+                # If this point is closer than the kth farthest,
+                # discard the farthest point and add this one
+                heapq.heappushpop(heap, (dist, i))
+        
+        # print(heap)
+        # Return all points stored in the max heap
+        return [points[i] for (_, i) in heap]
+    
+    def squared_distance(self, point: List[int]) -> int:
+        return point[0] ** 2 + point[1] ** 2
