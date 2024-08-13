6c181b2296d94bacf267be58cd90ead624d25cb2
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-13 18:21:44 +0530

    Time: 42 ms (94.37%), Space: 16.5 MB (94.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-
-        def dfs(target, stack, start):
+        candidates.sort(reverse=True)
+        def dfs(target, stack, index):
+            if target < 0:
+                return
             if target == 0:
-                result.append(list(stack))
-                return
-            elif target < 0:
-                return
+                result.append(stack.copy())
             
-            for idx in range(start, len(candidates)):
+            for idx in range(index, len(candidates)):
                 stack.append(candidates[idx])
-                dfs(target - candidates[idx], stack, idx)
+                dfs(target- candidates[idx], stack, idx)
                 stack.pop()
         
         dfs(target, [], 0)
