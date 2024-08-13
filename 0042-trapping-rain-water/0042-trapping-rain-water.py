4c6ca25a0a754fe2e2f097f7272125fd0d1ce206
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-13 12:44:07 +0530

    Time: 115 ms (26.73%), Space: 18.6 MB (8.76%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,23 +1,41 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        #edge case
-        if len(height) == 0:
-            return 0
         water = 0
-        left ,right = 0, len(height)-1
-        left_max, right_max = 0 , 0
-        while left < right:
-            if height[left] > left_max:
-                left_max = height[left]
-            if height[right] > right_max:
-                right_max = height[right]
+        idx = 0
+        stack = []
+        while idx < len(height):
             
-            #move the lower one for opposite max seen
-            if left_max > right_max:
-                water += max(0, right_max - height[right])
-                right -= 1
-            else:
-                water += max(0, left_max - height[left])
-                left += 1
+            while len(stack) != 0 and height[idx] > height[stack[-1]]:
+                peak = stack[-1]
+                stack.pop()
+                if len(stack) == 0:
+                    break
+                distance = idx - stack[-1] -1
+                water_height = min(height[idx], height[stack[-1]]) - height[peak]
+                water += distance * water_height
+                
+            stack.append(idx)
+            idx += 1
         return water
+    # def trap(self, height: List[int]) -> int:
+    #     #edge case
+    #     if len(height) == 0:
+    #         return 0
+    #     water = 0
+    #     left ,right = 0, len(height)-1
+    #     left_max, right_max = 0 , 0
+    #     while left < right:
+    #         if height[left] > left_max:
+    #             left_max = height[left]
+    #         if height[right] > right_max:
+    #             right_max = height[right]
+            
+    #         #move the lower one for opposite max seen
+    #         if left_max > right_max:
+    #             water += max(0, right_max - height[right])
+    #             right -= 1
+    #         else:
+    #             water += max(0, left_max - height[left])
+    #             left += 1
+    #     return water
             
