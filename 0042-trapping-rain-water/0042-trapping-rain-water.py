92daee319901598b388d95fe0b70127f48cd5c5c
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-13 12:49:03 +0530

    Time: 112 ms (33.1%), Space: 18.4 MB (80.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,22 +1,37 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        water = 0
-        idx = 0
-        stack = []
-        while idx < len(height):
-            
-            while len(stack) != 0 and height[idx] > height[stack[-1]]:
-                peak = stack[-1]
-                stack.pop()
-                if len(stack) == 0:
-                    break
-                distance = idx - stack[-1] -1
-                water_height = min(height[idx], height[stack[-1]]) - height[peak]
-                water += distance * water_height
-                
-            stack.append(idx)
-            idx += 1
+        left, right = 0, len(height) -1
+        water  = 0
+        left_max, right_max = 0, 0
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+                right -= 1
         return water
+    # def trap(self, height: List[int]) -> int:
+        # water = 0
+        # idx = 0
+        # stack = []
+        # while idx < len(height):
+            
+        #     while len(stack) != 0 and height[idx] > height[stack[-1]]:
+        #         peak = stack[-1]
+        #         stack.pop()
+        #         if len(stack) == 0:
+        #             break
+        #         distance = idx - stack[-1] -1
+        #         water_height = min(height[idx], height[stack[-1]]) - height[peak]
+        #         water += distance * water_height
+                
+        #     stack.append(idx)
+        #     idx += 1
+        # return water
+
     # def trap(self, height: List[int]) -> int:
     #     #edge case
     #     if len(height) == 0:
