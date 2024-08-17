85432c945d802b82ffbf55a5a753c441595dfd04
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-18 00:35:06 +0530

    Time: 646 ms (13.43%), Space: 22 MB (66.78%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,5 +1,21 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        map = {}
+        sum_val, max_len = 0, 0
+        for idx, num in enumerate(nums):
+            if num == 1:
+                sum_val += 1
+            else:
+                sum_val -=1
+            
+            if sum_val == 0:
+                max_len = idx + 1
+            elif sum_val in map:
+                max_len = max(max_len, idx - map[sum_val])
+            else:
+                map[sum_val] = idx
+        return max_len
+"""
         count_0, count_1 = 0, 0
         number_map = {}
         number_map[0] = -1
@@ -17,3 +33,4 @@ class Solution:
                 answer = max(answer, idx - number_map[count_1 - count_0])
             # print(idx, number_map,count_0, count_1, nums, answer)
         return answer
+"""
