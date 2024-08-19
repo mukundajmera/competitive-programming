8c78e72eddb80cb2d1cc6af266a2d1c614eef8ca
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-19 21:49:09 +0530

    Time: 36 ms (83.71%), Space: 16.6 MB (61.96%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,9 +1,19 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        dp = [float('inf')] * (n + 1)
-        dp[1] = 0
-        for idx in range(2, n+1):
-            for idj in range(1, (idx // 2) + 1):
-                if idx % idj == 0:
-                    dp[idx] = min(dp[idx], dp[idj] + (idx// idj))
-        return dp[n]
+        ans = 0
+        d = 2
+        while n > 1:
+            # If d is prime factor, keep dividing
+            # n by d until is no longer divisible
+            while n % d == 0:
+                ans += d
+                n //= d
+            d += 1
+        return ans
+        # dp = [float('inf')] * (n + 1)
+        # dp[1] = 0
+        # for idx in range(2, n+1):
+        #     for idj in range(1, (idx // 2) + 1):
+        #         if idx % idj == 0:
+        #             dp[idx] = min(dp[idx], dp[idj] + (idx// idj))
+        # return dp[n]
