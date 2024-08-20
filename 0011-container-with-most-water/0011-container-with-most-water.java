b74ac8bd1359d8c78ff48c88f051adaff032ee65
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-20 20:55:43 +0530

    Time: 2 ms (98.76%), Space: 57.9 MB (36.63%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -8,12 +8,31 @@ class Solution {
             int current_height = Math.min(height[leftIdx], height[rightIdx]);
             maxWater = Math.max(maxWater, current_height * width);
 
-            if(height[leftIdx] < height[rightIdx]){
+            while(leftIdx < rightIdx && height[leftIdx] <= current_height){
                 leftIdx++;
-            }else{
+            }
+
+            while(leftIdx < rightIdx && height[rightIdx] <= current_height){
                 rightIdx--;
             }
         }
         return maxWater;
     }
+    // public int maxArea(int[] height) {
+    //     int maxWater = 0;
+    //     int leftIdx = 0, rightIdx = height.length-1;
+
+    //     while(leftIdx < rightIdx){
+    //         int width = rightIdx -  leftIdx;
+    //         int current_height = Math.min(height[leftIdx], height[rightIdx]);
+    //         maxWater = Math.max(maxWater, current_height * width);
+
+    //         if(height[leftIdx] < height[rightIdx]){
+    //             leftIdx++;
+    //         }else{
+    //             rightIdx--;
+    //         }
+    //     }
+    //     return maxWater;
+    // }
 }
