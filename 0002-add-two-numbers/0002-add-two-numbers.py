35f92f800c39679f8c9964ff62e0871c5e451f1f
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-21 21:26:31 +0530

    Time: 43 ms (97.06%), Space: 16.6 MB (72.24%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1 and not l2:
+            return
+        result = None
+        curr = None
+        p1, p2 = l1, l2
+        carry = False
+        while p1 or p2:
+            sum = 0
+            if p1:
+                sum += p1.val
+                p1 = p1.next
+            if p2:
+                sum += p2.val
+                p2 = p2.next
+
+            if carry:
+                sum += 1
+                carry = False
+
+            digit = sum%10
+            
+            if sum >= 10:
+                carry = True
+            node = ListNode(digit)
+            if not result:
+                result = node
+                curr = result
+            else:
+                curr.next = node
+                curr = curr.next
+        if carry:
+            curr.next = ListNode(1)
+        return result
+            
