04d30ed1803cddcc9792a52365a6270a92fee104
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-26 02:39:01 +0530

    Time: 0 ms (100%), Space: 44.7 MB (63.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int index = 0;
-        Set<Integer> visited = new HashSet<>();
-        for(int i = 0; i < nums.length;i++){
-            if(!visited.contains(nums[i])){
-                visited.add(nums[i]);
+        int index = 1;
+        for(int i = 1; i < nums.length;i++){
+            if(nums[i] != nums[i-1]){
                 nums[index] = nums[i];
                 index++;
             }
         }
         return index;
+        // int index = 0;
+        // Set<Integer> visited = new HashSet<>();
+        // for(int i = 0; i < nums.length;i++){
+        //     if(!visited.contains(nums[i])){
+        //         visited.add(nums[i]);
+        //         nums[index] = nums[i];
+        //         index++;
+        //     }
+        // }
+        // return index;
     }
 }
