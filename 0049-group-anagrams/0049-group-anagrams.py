3af2edb209e515f1d64f10b9fe12bade5069ee82
Mukund Ajmera <mukundajmera94@gmail.com>
2024-08-29 15:43:12 +0530

    Time: 102 ms (16.57%), Space: 22.3 MB (17.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,25 @@
 class Solution:            
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = []
-        char_map = {}
-        for word in strs:
-            # value = sorted(word)
-            value = "".join(sorted(word))
-            # print(word, value)
-            if value in char_map:
-                char_map[value].append(word)
-            else:
-                char_map[value] = [word]
-        for value in char_map.values():
-            groups.append(value)
-        return groups
+        answer = defaultdict(list)
+        for string in strs:
+            prefix = [0] * 26
+            for ch in string:
+                prefix[ord(ch) - ord('a')] += 1
+            answer[tuple(prefix)].append(string)
+        
+        return answer.values()
+
+
+        # groups = []
+        # char_map = {}
+        # for word in strs:
+        #     # value = sorted(word)
+        #     value = "".join(sorted(word))
+        #     # print(word, value)
+        #     if value in char_map:
+        #         char_map[value].append(word)
+        #     else:
+        #         char_map[value] = [word]
+        # for value in char_map.values():
+        #     groups.append(value)
+        # return groups
