80224a8b883b1e09aac5bf9229873c1a8f2b46c7
Mukund Ajmera <mukundajmera94@gmail.com>
2024-09-04 03:14:33 +0530

    Time: 113 ms (94.49%), Space: 20.6 MB (52.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,9 +4,10 @@ class Solution:
         intervals.sort(key=lambda x: x[0])
         n = len(intervals)
         for interval in intervals:
-            if not result or result[-1][1] < interval[0]:
+            if not result or (result[-1][1] < interval[0] or interval[1] < result[-1][0]):
                 result.append(interval)
             else:
+                result[-1][0] = min(result[-1][0], interval[0])
                 result[-1][1] = max(result[-1][1], interval[1])
         return result
             
