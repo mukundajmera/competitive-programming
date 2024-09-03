88339c7a21dc1b987d6c0e1cbe9f0420752c41e2
Mukund Ajmera <mukundajmera94@gmail.com>
2024-09-03 18:50:27 +0530

    Time: 115 ms (64.57%), Space: 23.4 MB (12.94%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,16 +1,29 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        max_cite = max(citations)
-        counts = [0] * (max_cite + 1)
+        #using BS
+        n = len(citations)
+        cite = 0
+        left, right = 0,  n -1
+        while left <= right:
+            mid = (left + right)//2
+            if citations[mid] >= n - mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return n - left
 
-        for cite in citations:
-            counts[cite] += 1
+        # max_cite = max(citations)
+        # counts = [0] * (max_cite + 1)
+
+        # for cite in citations:
+        #     counts[cite] += 1
         
 
-        total_paper = 0
-        for hidx in range(max_cite, -1, -1):
-            total_paper += counts[hidx]
-            if total_paper >= hidx:
-                return hidx
+        # total_paper = 0
+        # for hidx in range(max_cite, -1, -1):
+        #     total_paper += counts[hidx]
+        #     if total_paper >= hidx:
+        #         return hidx
         
-        return 0
+        # return 0
