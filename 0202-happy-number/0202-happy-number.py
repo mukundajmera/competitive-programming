8be7f7e9935f2033588d0ce943b1e02a30cf701d
Mukund Ajmera <mukundajmera94@gmail.com>
2024-09-05 02:23:50 +0530

    Time: 40 ms (34.86%), Space: 16.5 MB (72.88%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,19 +1,24 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        num_square = {i:i**2 for i in range(0,11)}
-        # print(num_square)
-        is_happy = False
-        numbers = n
-        execution = 1000
-        while execution > 0 :
-            current_sum = 0
-            while numbers > 0:
-                digit = numbers % 10
-                numbers //= 10
-                # print(type(digit), digit, num_square.get(digit))
-                current_sum += num_square.get(digit)
-            if current_sum == 1:
-                return True
-            numbers = current_sum
-            execution -= 1
-        return False
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum([int(idx) ** 2 for idx in str(n)])
+        return n == 1
+        # num_square = {i:i**2 for i in range(0,11)}
+        # # print(num_square)
+        # is_happy = False
+        # numbers = n
+        # execution = 1000
+        # while execution > 0 :
+        #     current_sum = 0
+        #     while numbers > 0:
+        #         digit = numbers % 10
+        #         numbers //= 10
+        #         # print(type(digit), digit, num_square.get(digit))
+        #         current_sum += num_square.get(digit)
+        #     if current_sum == 1:
+        #         return True
+        #     numbers = current_sum
+        #     execution -= 1
+        # return False
