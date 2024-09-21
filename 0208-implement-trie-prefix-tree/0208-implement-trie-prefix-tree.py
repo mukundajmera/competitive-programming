6026cd5cdae803a9daf6f30905aec3a5381a6ba0
Mukund Ajmera <mukundajmera94@gmail.com>
2024-09-22 03:52:32 +0530

    Time: 105 ms (91.89%), Space: 29.5 MB (76.87%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,38 +1,69 @@
 class Trie:
 
     def __init__(self):
-        self.trie = {"*" : ""}
+        self.trie = {}
 
     def insert(self, word: str) -> None:
-        current = self.trie
+        current_tree = self.trie
         for ch in word:
-            if not ch in current:
-                current[ch] = {}
-            current = current[ch]
-        print(current)
-        current["*"] = {} 
-
+            if ch not in current_tree:
+                current_tree[ch] = {}
+            current_tree = current_tree[ch]
+        #after all going to leef node
+        current_tree['*'] = ''
+    #check for traversing entire word and check if leaf node is '*'
     def search(self, word: str) -> bool:
-        found = True
-        current = self.trie
-        # print(current)
+        tree = self.trie
         for ch in word:
-            if not ch in current:
+            if ch not in tree:
                 return False
-            else:
-                current = current[ch]
-        if current.get("*") is None:
-            found = False
-        return found
-
+            tree = tree[ch]
+        return '*' in tree
+    
+        
+    #check for traversing partial tree
     def startsWith(self, prefix: str) -> bool:
-        found = True
-        current = self.trie
+        tree = self.trie
         for ch in prefix:
-            if ch not in current:
+            if ch not in tree:
                 return False
-            current = current[ch]
+            tree = tree[ch]
         return True
+# class Trie:
+
+#     def __init__(self):
+#         self.trie = {"*" : ""}
+
+#     def insert(self, word: str) -> None:
+#         current = self.trie
+#         for ch in word:
+#             if not ch in current:
+#                 current[ch] = {}
+#             current = current[ch]
+#         print(current)
+#         current["*"] = {} 
+
+#     def search(self, word: str) -> bool:
+#         found = True
+#         current = self.trie
+#         # print(current)
+#         for ch in word:
+#             if not ch in current:
+#                 return False
+#             else:
+#                 current = current[ch]
+#         if current.get("*") is None:
+#             found = False
+#         return found
+
+#     def startsWith(self, prefix: str) -> bool:
+#         found = True
+#         current = self.trie
+#         for ch in prefix:
+#             if ch not in current:
+#                 return False
+#             current = current[ch]
+#         return True
 
 
 # Your Trie object will be instantiated and called as such:
