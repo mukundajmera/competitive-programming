5d7e2d91662a05d21302d7e4d25e568a939cebce
Mukund Ajmera <mukundajmera94@gmail.com>
2024-09-22 17:46:51 +0530

    Time: 43 ms (92.88%), Space: 16.6 MB (44.24%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,23 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
-        candidates.sort(reverse=True)
-        def dfs(target, stack, index):
-            if target < 0:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        combinations = []
+        def dfs(index, stack, current_sum):
+            if current_sum == target:
+                combinations.append(stack.copy())
                 return
-            if target == 0:
-                result.append(stack.copy())
-            
-            for idx in range(index, len(candidates)):
-                stack.append(candidates[idx])
-                dfs(target- candidates[idx], stack, idx)
+
+            if current_sum > target or index >= len(nums):
+                return
+
+            for idx in range(index, len(nums)):
+                stack.append(nums[idx])
+                dfs(idx, stack, current_sum + nums[idx])
                 stack.pop()
+
+            # stack.append(nums[index])
+            # dfs(index, stack, current_sum + nums[index])
+            # stack.pop()
+            # dfs(index + 1, stack, current_sum)
         
-        dfs(target, [], 0)
-        return result
+        dfs(0, [], 0)
+        return combinations
