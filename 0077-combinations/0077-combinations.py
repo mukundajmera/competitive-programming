07220bd33c98e0a573c4b2fb31e7eb52ce78234f
Mukund Ajmera <mukundajmera94@gmail.com>
2024-09-27 00:14:49 +0530

    Time: 763 ms (20.93%), Space: 64.7 MB (71.96%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        combination = []
+        def dfs(index, stack):
+            if len(stack) == k:
+                combination.append(stack[:])
+                return
+
+            for idx in range(index, n+1):
+                stack.append(idx)
+                dfs(idx + 1, stack)
+                stack.pop()
+        
+        dfs(1, [])
+        return combination
