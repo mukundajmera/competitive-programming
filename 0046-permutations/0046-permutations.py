a438bb1c3ecc877d48ae060edcf8c05fb46c1248
Mukund Ajmera <mukundajmera94@gmail.com>
2024-09-27 00:26:59 +0530

    Time: 44 ms (29.92%), Space: 16.8 MB (34.11%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        def dfs(stack):
-            if len(nums) == len(stack):
-                result.append(stack.copy())
-            
-            for idx in range(len(nums)):
+        combination = []
+        def dfs(index, stack):
+            if index == len(nums):
+                combination.append(stack[:])
+                return
+
+            for idx in range(0, len(nums)):
                 if nums[idx] not in stack:
                     stack.append(nums[idx])
-                    dfs(stack)
+                    dfs(index + 1, stack)
                     stack.pop()
         
-        dfs([])
-        return result
+        dfs(0, [])
+        return combination
