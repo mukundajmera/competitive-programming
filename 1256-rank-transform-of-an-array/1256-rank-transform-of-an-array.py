d2e902089023e2dbe9f2fcaeb89344bedfd97622
Mukund Ajmera <mukundajmera94@gmail.com>
2024-10-02 23:52:51 +0530

    Time: 274 ms (13.65%), Space: 47.1 MB (5.11%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,12 +1,26 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        num_to_rank = {}
-        sorted_arr = sorted(arr)
+        # Store the rank for each number in arr
+        num_to_indices = {k: [] for k in sorted(set(arr))}
+
+        for i, num in enumerate(arr):
+            num_to_indices[num].append(i)
+
         rank = 1
-        for i in range(len(sorted_arr)):
-            if i > 0 and sorted_arr[i] > sorted_arr[i - 1]:
-                rank += 1
-            num_to_rank[sorted_arr[i]] = rank
-        for i in range(len(arr)):
-            arr[i] = num_to_rank[arr[i]]
+        for num in num_to_indices.keys():
+            for index in num_to_indices[num]:
+                arr[index] = rank
+            rank += 1
+
         return arr
+
+        # num_to_rank = {}
+        # sorted_arr = sorted(arr)
+        # rank = 1
+        # for i in range(len(sorted_arr)):
+        #     if i > 0 and sorted_arr[i] > sorted_arr[i - 1]:
+        #         rank += 1
+        #     num_to_rank[sorted_arr[i]] = rank
+        # for i in range(len(arr)):
+        #     arr[i] = num_to_rank[arr[i]]
+        # return arr
