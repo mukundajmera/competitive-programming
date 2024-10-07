c98ee439546266d4ab154760b5590f89dfa7328a
Mukund Ajmera <mukundajmera94@gmail.com>
2024-10-07 23:24:34 +0530

    Time: 48 ms (34.19%), Space: 16.6 MB (7.81%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -1,15 +1,30 @@
 class Solution:
-    def remove_substring(self, string: str, pattern):
-        index = string.find(pattern)
-        # print(string, index, len(string))
-        string = string[:index] + string[index+2:]
-        return string
+    # def remove_substring(self, string: str, pattern):
+    #     index = string.find(pattern)
+    #     # print(string, index, len(string))
+    #     string = string[:index] + string[index+2:]
+    #     return string
         
     def minLength(self, s: str) -> int:
-        while "AB" in s or "CD" in s:
-            if "AB" in s:
-                s = self.remove_substring(s, "AB")
-            if "CD" in s:
-                s = self.remove_substring(s, "CD")
+        stack = []
+
+        for ch in s:
+            if not stack:
+                stack.append(ch)
+                continue
+            
+            if ch == "B" and stack[-1] == "A":
+                stack.pop()
+            elif ch == "D" and stack[-1] == "C":
+                stack.pop()            
+            else:
+                stack.append(ch)
+
+        return len(stack)
+        # while "AB" in s or "CD" in s:
+        #     if "AB" in s:
+        #         s = self.remove_substring(s, "AB")
+        #     if "CD" in s:
+        #         s = self.remove_substring(s, "CD")
         
-        return len(s)
+        # return len(s)
