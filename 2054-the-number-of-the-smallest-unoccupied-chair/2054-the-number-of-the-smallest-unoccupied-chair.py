8029ea7bf089212f2ca0d5104deb092e707e7981
Mukund Ajmera <mukundajmera94@gmail.com>
2024-10-11 17:54:48 +0530

    Time: 547 ms (86.49%), Space: 22.7 MB (76.28%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,18 +1,48 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        # times.sort(key=lambda time: time[0])
-        target_friend = times[targetFriend]
-        times.sort()
+        target_arrival = times[targetFriend][0]
+        times = sorted([(arrival, leave, index) for index, (arrival, leave) in enumerate(times)])
 
-        n = len(times)
-        chair_timer = [0] * n
+        next_chair = 0
+        available_chairs = []
+        leaving_queue = []
 
         for time in times:
-            for idx in range(n):
-                if chair_timer[idx] <= time[0]:
-                    chair_timer[idx] = time[1]
-                    if time == target_friend:
-                        return idx
-                    break
+            arrival, leave, index = time
 
+            # Free up chairs based on current time
+            while leaving_queue and leaving_queue[0][0] <= arrival:
+                _, chair = heapq.heappop(leaving_queue)
+                heapq.heappush(available_chairs, chair)
+
+            #utiilise from available chair first
+            if available_chairs:
+                current_chair = heapq.heappop(available_chairs)
+            else:
+                current_chair = next_chair
+                next_chair += 1
+
+            # Push current leave time and chair
+            heapq.heappush(leaving_queue, (leave, current_chair))
+
+            # Check if it's the target friend
+            if index == targetFriend:
+                return current_chair
         return 0
+
+        # # times.sort(key=lambda time: time[0])
+        # target_friend = times[targetFriend]
+        # times.sort()
+
+        # n = len(times)
+        # chair_timer = [0] * n
+
+        # for time in times:
+        #     for idx in range(n):
+        #         if chair_timer[idx] <= time[0]:
+        #             chair_timer[idx] = time[1]
+        #             if time == target_friend:
+        #                 return idx
+        #             break
+
+        # return 0
