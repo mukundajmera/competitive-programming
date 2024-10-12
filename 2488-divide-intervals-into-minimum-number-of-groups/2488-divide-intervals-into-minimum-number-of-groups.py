9be19a5c3d0c5674111259fa310876c890c1d31d
Mukund Ajmera <mukundajmera94@gmail.com>
2024-10-12 19:40:18 +0530

    Time: 995 ms (98.1%), Space: 55.9 MB (72.15%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
@@ -1,18 +1,30 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        events = []
-        for interval in intervals:
-            events.append((interval[0], 1))
-            events.append((interval[1] + 1, -1))
-        
-        events.sort(key=lambda event: (event[0], event[1]))
+        start = sorted(idx[0] for idx in intervals)
+        end = sorted(idx[1] for idx in intervals)
 
-        max_overlap = 0
-        overlap = 0
+        end_idx, group_count = 0, 0
 
-        for event in events:
-            overlap += event[1]
+        for index in start:
+            if index > end[end_idx]:
+                end_idx += 1
+            else:
+                group_count += 1
         
-            max_overlap = max(max_overlap, overlap)
+        return group_count
+        # events = []
+        # for interval in intervals:
+        #     events.append((interval[0], 1))
+        #     events.append((interval[1] + 1, -1))
         
-        return max_overlap
+        # events.sort(key=lambda event: (event[0], event[1]))
+
+        # max_overlap = 0
+        # overlap = 0
+
+        # for event in events:
+        #     overlap += event[1]
+        
+        #     max_overlap = max(max_overlap, overlap)
+        
+        # return max_overlap
