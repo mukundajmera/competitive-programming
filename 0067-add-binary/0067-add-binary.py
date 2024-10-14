6a232330c88d165dc0f0a5de3389ec9517df62d4
Mukund Ajmera <mukundajmera94@gmail.com>
2024-10-14 19:06:26 +0530

    Time: 36 ms (63.92%), Space: 16.7 MB (28.02%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,62 +1,19 @@
 class Solution:
-    def addBinary(self, a, b) -> str:
-        answer = []
+    def addBinary(self, a: str, b: str) -> str:
+        s = []
         carry = 0
-        ida, idb = len(a)-1, len(b)-1
-        while ida >= 0 or idb >= 0 or carry:
-            if ida >= 0:
-                carry += int(a[ida])
-                ida -= 1
-            if idb >= 0:
-                carry += int(b[idb])
-                idb -= 1
-            answer.append(str(carry % 2))
-            carry //= 2
-        return "".join(answer[::-1])
+        alen, blen = len(a), len(b)
+
+        while alen > 0 or blen > 0 or carry:
+            if alen > 0:
+                carry += int(a[alen-1])
+                alen -= 1
             
-
-    # def addBinary(self, a, b) -> str:
-    #     n = max(len(a), len(b))
-    #     a, b = a.zfill(n), b.zfill(n)
+            if blen > 0:
+                carry += int(b[blen-1])
+                blen -= 1
+            
+            s.append(str(carry % 2))
+            carry //= 2
         
-    #     answer = []
-    #     count = 0
-    #     for idx in reversed(range(n)):
-    #         if a[idx] == "1":
-    #             count += 1
-    #         if b[idx] == "1":
-    #             count += 1
-    #         if count % 2 == 1:
-    #             answer.append('1')
-    #         else:
-    #             answer.append('0')
-    #         count = count // 2
-    #     if count == 1:
-    #         answer.append('1')
-    #     return "".join(answer[::-1])
-
-
-    # def addBinary(self, a, b) -> str:
-    #     n = max(len(a), len(b))
-    #     a, b = a.zfill(n), b.zfill(n)
-
-    #     carry = 0
-    #     answer = []
-    #     for i in range(n - 1, -1, -1):
-    #         if a[i] == "1":
-    #             carry += 1
-    #         if b[i] == "1":
-    #             carry += 1
-
-    #         if carry % 2 == 1:
-    #             answer.append("1")
-    #         else:
-    #             answer.append("0")
-
-    #         carry //= 2
-
-    #     if carry == 1:
-    #         answer.append("1")
-    #     answer.reverse()
-
-    #     return "".join(answer)
+        return "".join(reversed(s))
