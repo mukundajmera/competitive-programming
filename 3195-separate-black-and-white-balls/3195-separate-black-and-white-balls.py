157031b86556dcbda52b846273da826d6230d16f
Mukund Ajmera <mukundajmera94@gmail.com>
2024-10-15 19:37:59 +0530

    Time: 75 ms (93.43%), Space: 17.6 MB (54.03%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -1,30 +1,37 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        left, right = 0, len(s)-1
-        while left < len(s):
-            if s[left] != '0':
-                break
-            left += 1
+        swap, black = 0, 0
+        for ch in s:
+            if ch == "0":
+                swap += black
+            else:
+                black += 1
+        return swap
+        # left, right = 0, len(s)-1
+        # while left < len(s):
+        #     if s[left] != '0':
+        #         break
+        #     left += 1
 
-        while right >= 0:
-            if s[right] != '1':
-                break
-            right -= 1
+        # while right >= 0:
+        #     if s[right] != '1':
+        #         break
+        #     right -= 1
         
-        count = 0
-        while left < right:
-            if s[left] == '1' and s[right] == '0':
-                count += right - left
-                left += 1
-                right -= 1
-                continue
+        # count = 0
+        # while left < right:
+        #     if s[left] == '1' and s[right] == '0':
+        #         count += right - left
+        #         left += 1
+        #         right -= 1
+        #         continue
             
-            while left < len(s) and s[left] == '0':
-                left += 1
+        #     while left < len(s) and s[left] == '0':
+        #         left += 1
             
-            while right >= 0 and s[right] == '1':
-                right -= 1
+        #     while right >= 0 and s[right] == '1':
+        #         right -= 1
         
-        return count
+        # return count
 
             
