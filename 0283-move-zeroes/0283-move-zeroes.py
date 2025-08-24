393eefef89d220bb43423ae6d688fb9af8015ab7
Mukund Ajmera <mukundajmera94@gmail.com>
2025-08-24 19:23:11 +0530

    Time: 2094 ms (5%), Space: 19.1 MB (9.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,7 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # left, right = 0,0
+        left, right = 0, 0
+
+        while left < len(nums) and right < len(nums):
+            
+            # move till next move 
+            while left < len(nums) and nums[left] != 0:
+                left += 1
+            right = left
+
+            while right < len(nums) and nums[right] == 0:
+                right += 1
+            
+            # print(left, right)
+            #swap it
+            if right != len(nums):
+                nums[left], nums[right] = nums[right], nums[left]
+            
+            left += 1
+        
+        """
+                # left, right = 0,0
         # while left < len(nums):
         #     if nums[left] != 0:
         #         nums[left], nums[right] = nums[right], nums[left]
@@ -14,3 +34,6 @@ class Solution:
             if nums[idx] != 0:
                 nums[idx], nums[zero_idx] = nums[zero_idx], nums[idx]
                 zero_idx += 1
+        """
+            
+
