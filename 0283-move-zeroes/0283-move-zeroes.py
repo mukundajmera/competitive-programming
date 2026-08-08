13308be7a2b4930748caa3f9fd609acf6791f583
Mukund Ajmera <mukundajmera94@gmail.com>
2026-08-09 01:39:21 +0530

    0283-MOVE-ZEROES Stats: Time: 5 ms (49.1%), Space: 20.4 MB (89.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,37 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left, right = 0, 0
-
-        while left < len(nums) and right < len(nums):
-            
-            # move till next move 
-            while left < len(nums) and nums[left] != 0:
-                left += 1
-            right = left
-
-            while right < len(nums) and nums[right] == 0:
-                right += 1
-            
-            # print(left, right)
-            #swap it
-            if right != len(nums):
-                nums[left], nums[right] = nums[right], nums[left]
-            
-            left += 1
-        
-        """
-        # left, right = 0,0
-        # while left < len(nums):
-        #     if nums[left] != 0:
-        #         nums[left], nums[right] = nums[right], nums[left]
-        #         right += 1
-        #     left += 1
-        zero_idx = 0
+        next_zero = 0
         for idx in range(len(nums)):
             if nums[idx] != 0:
-                nums[idx], nums[zero_idx] = nums[zero_idx], nums[idx]
-                zero_idx += 1
-        """
-            
-
+                nums[next_zero], nums[idx] = nums[idx], nums[next_zero]
+                next_zero += 1
