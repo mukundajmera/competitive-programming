e0cd52c0622abe45c1cfc0f94ffeaaa6de0f9671
Mukund Ajmera <mukundajmera94@gmail.com>
2026-08-13 01:00:05 +0530

    0141-LINKED-LIST-CYCLE Stats: Time: 53 ms (62.4%), Space: 22.4 MB (86.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -8,10 +8,11 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
             return False
-        slow, fast = head, head.next
-        while slow != fast:
-            if not fast or not fast.next:
-                return False
+        
+        slow, fast = head, head
+        while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-        return True
+            if slow == fast:
+                return True
+        return False
--- README.md
+++ README.md
@@ -1098,4 +1098,8 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 |  |
 | ------- |
 | [0084-largest-rectangle-in-histogram](https://github.com/mukundajmera/competitive-programming/tree/master/0084-largest-rectangle-in-histogram) |
+## Floyd's Cycle Finding Algorithm
+|  |
+| ------- |
+| [0141-linked-list-cycle](https://github.com/mukundajmera/competitive-programming/tree/master/0141-linked-list-cycle) |
 <!---LeetCode Topics End-->
