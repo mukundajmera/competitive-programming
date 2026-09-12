f6a5633b7fe1fbc8a4f516e5a3b4f73ab35fd9d0
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-13 02:15:08 +0530

    0098-VALIDATE-BINARY-SEARCH-TREE Stats: Time: 0 ms (100%), Space: 20.8 MB (75.15%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,14 +6,24 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node, max_val, min_val):
-            if not node:
+        def dfs(node, min_, max_):
+            # base case
+            if node is None:
                 return True
+            # check if the current node's value is within the valid range
+            if node.val <= min_ or node.val >= max_:
+                return False
+            return dfs(node.left, min_, node.val) and dfs(node.right, node.val, max_)
+        return dfs(root, float('-inf'), float('inf'))
+
+        # def dfs(node, max_val, min_val):
+        #     if not node:
+        #         return True
             
-            is_valid = False
-            if min_val < node.val < max_val:
-                is_valid = True
+        #     is_valid = False
+        #     if min_val < node.val < max_val:
+        #         is_valid = True
             
-            return is_valid and dfs(node.left, node.val, min_val) and dfs(node.right, max_val, node.val)
+        #     return is_valid and dfs(node.left, node.val, min_val) and dfs(node.right, max_val, node.val)
         
-        return dfs(root,  float('inf'), float('-inf'))   
+        # return dfs(root,  float('inf'), float('-inf'))   
