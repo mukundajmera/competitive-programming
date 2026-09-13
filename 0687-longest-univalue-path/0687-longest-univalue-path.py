425216eaa053138ae8cb6d8f8fa4b87a83da77b0
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-13 22:00:37 +0530

    0687-LONGEST-UNIVALUE-PATH Stats: Time: 35 ms (77.84%), Space: 22.2 MB (17.2%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -20,3 +20,27 @@ class Solution:
 
         dfs(root, TreeNode(val=0))
         return max_value
+        # max_value = 0
+
+        # def dfs(node):
+        #     if not node:
+        #         return 0
+        #     nonlocal max_value
+            
+        #     left_length = dfs(node.left)
+        #     right_length = dfs(node.right)
+        #     left_arrow = right_arrow = 0
+
+        #     # check if children have the same value as the current node,
+        #     # which means we can extend the univalue path by including the
+        #     # current node
+        #     if node.left and node.left.val == node.val:
+        #         left_arrow = left_length + 1
+        #     if node.right and node.right.val == node.val:
+        #         right_arrow = right_length + 1
+
+        #     max_value = max(max_value, left_arrow + right_arrow)
+        #     return max(left_arrow, right_arrow)
+            
+        # dfs(root)
+        # return max_value
