5e5e084751e267ca4e0d8fabfaf59a0c666f4934
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-14 01:39:37 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 239 ms (68.71%), Space: 21.7 MB (48.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -22,5 +22,38 @@ class Solution:
                 if grid[row][col] == "1":
                     count += 1
                     dfs(row,col)
-        
         return count
+
+        # nr = len(grid)
+        # nc = len(grid[0])
+        # count = 0
+        # for r in range(0, nr):
+        #     for c in range(0, nc):
+        #         if grid[r][c] == '1':
+        #             count += 1
+        #             neighbors = []
+        #             neighbors.append((r, c))
+        #             while neighbors:
+        #                 row, col = neighbors.pop(0)
+        #                 if row - 1 >= 0 and grid[row - 1][col] == "1":
+        #                     neighbors.append((row - 1, col))
+        #                     grid[row - 1][col] = "0"
+        #                 if row + 1 < nr and grid[row + 1][col] == "1":
+        #                     neighbors.append((row + 1, col))
+        #                     grid[row + 1][col] = "0"
+        #                 if col - 1 >= 0 and grid[row][col - 1] == "1":
+        #                     neighbors.append((row, col - 1))
+        #                     grid[row][col - 1] = "0"
+        #                 if col + 1 < nc and grid[row][col + 1] == "1":
+        #                     neighbors.append((row, col + 1))
+        #                     grid[row][col + 1] = "0"
+        #         # queue = Deque([(row,col)])
+        #         # while queue:
+        #         #     row, col = queue.popleft()
+        #         #     grid[row][col] = 0
+        #         #     for nr, nc in directions:
+        #         #         nr += row
+        #         #         nc += col
+        #         #         if 0 <= nr < nrows and 0 <= nc < ncols and grid[nr][nc] == '1':
+        #         #             queue.append((nr,nc))
+        # return count
