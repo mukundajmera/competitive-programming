4c74b28c4db79836e5effb2bda876bfd372c3517
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-15 23:26:43 +0530

    1036-ROTTING-ORANGES Stats: Time: 3 ms (75.61%), Space: 19.3 MB (79.52%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,35 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        minute = -1
-        rot = deque()
-        has_orange = False            # whether grid contain an orange
-        for x in range(m):
-            for y in range(n):
-                has_orange = has_orange or grid[x][y]
-                if grid[x][y] == 2:   # rotten orange
-                    rot.append((x, y))
-        if not has_orange: return 0   # no orange in grid, return 0
-        while rot:          # bfs to rot oranges
-            minute += 1
-            count = len(rot)
-            for _ in range(count):
-                x, y = rot.popleft()
-                if x+1 < m and grid[x+1][y] == 1:
-                        grid[x+1][y] = 2
-                        rot.append((x+1, y))
-                if x-1 >= 0 and grid[x-1][y] == 1:
-                        grid[x-1][y] = 2
-                        rot.append((x-1, y))
-                if y+1 < n and grid[x][y+1] == 1:
-                        grid[x][y+1] = 2
-                        rot.append((x, y+1))
-                if y-1 >= 0 and grid[x][y-1] == 1:
-                        grid[x][y-1] = 2
-                        rot.append((x, y-1))
-        for x in range(m):    # check for unrotten oranges
-            for y in range(n):
-                if grid[x][y] == 1:
-                    return -1
-        return minute
+        if not grid:
+            return -1
+        nrows, ncols = len(grid), len(grid[0])
+        queue = deque()
+        fresh_orange = 0
+
+        for row in range(nrows):
+            for col in range(ncols):
+                if grid[row][col] == 2:
+                    queue.append((row, col))
+                elif grid[row][col] == 1:
+                    fresh_orange += 1
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        minutes = 0
+
+        while queue and fresh_orange > 0:
+            minutes += 1
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
+                for nr, nc in directions:
+                    nr += row
+                    nc += col
+                    if 0 <= nr < nrows and 0 <= nc < ncols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh_orange -= 1
+                        queue.append((nr,nc))
+
+        return minutes if fresh_orange == 0 else -1
