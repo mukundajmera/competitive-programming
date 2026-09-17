88637d43764b7696f435631f730956b57106c98f
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-18 00:18:21 +0530

    0542-01-MATRIX Stats: Time: 95 ms (88.82%), Space: 22.3 MB (49.02%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -22,3 +22,26 @@ class Solution:
                         queue.append((nr, nc))
             distance += 1
         return output
+
+        # matrix = [[0] * len(mat[0]) for row in mat]
+        # nrow, ncol = len(mat), len(mat[0])
+        # queue = Deque()
+        # visited = set()
+        # step = 0
+        # for row in range(nrow):
+        #     for col in range(ncol):
+        #         if mat[row][col] == 0:
+        #             queue.append((row,col,step))
+        #             visited.add((row,col))
+        
+        # directions = [(0,1), (0, -1), (-1, 0), (1, 0)]
+        # while queue:
+        #     row, col, step = queue.popleft()
+        #     for newr, newc in directions:
+        #         newr += row
+        #         newc += col
+        #         if 0 <= newr < nrow and 0 <= newc < ncol and (newr, newc) not in visited:
+        #             visited.add((newr,newc))
+        #             matrix[newr][newc] = step + 1
+        #             queue.append((newr, newc, step+1))
+        # return matrix
