d557478bcaad28b789dad3c2880da9d3578e3c79
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-21 00:32:54 +0530

    0054-SPIRAL-MATRIX Stats: Time: 0 ms (100%), Space: 19.4 MB (35.3%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,34 +1,35 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        if not matrix or not matrix[0]:
+            return []
         result = []
-        rows, cols = len(matrix), len(matrix[0])
-        up = left = 0
-        right = cols - 1
-        down = rows - 1
+        nrow, ncol = len(matrix), len(matrix[0])
+        left, right = 0, ncol - 1
+        up, down = 0, nrow - 1
 
-        while len(result) < rows * cols:
-            # travers to right
-            for col in range(left, right+1):
+        while len(result) < nrow * ncol:
+            #right
+            for col in range(left, right + 1):
                 result.append(matrix[up][col])
 
-            # traverse to down
-            for row in range(up+1, down+1):
+            #down
+            for row in range(up + 1, down + 1):
                 result.append(matrix[row][right])
-                
+
             if up == down:
                 break
 
-            # traverse to left
-            for col in range(right -1, left-1, -1):
+            #left
+            for col in range(right - 1, left - 1, -1):
                 result.append(matrix[down][col])
 
             if left == right:
                 break
 
-            # traverse to up
+            #up
             for row in range(down - 1, up, -1):
                 result.append(matrix[row][left])
-                
+            
             left += 1
             right -= 1
             up += 1
