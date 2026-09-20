f20e3314a78ae90aab68714af1335c49f082f738
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-20 23:14:14 +0530

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: 35 ms (46.4%), Space: 21.8 MB (57.01%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,28 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        #using hash map
-        sub_map = {}
-        sub_map[0] = 1
-        sum  = 0
+        subarray = 0
         count = 0
-        for idx in range(len(nums)):
-            sum += nums[idx]
-            if (sum - k) in  sub_map:
-                # print("i am in")
-                count += sub_map[sum-k]
-            sub_map[sum] = sub_map.get(sum,0) + 1
-            # print(sub_map, sum-k)
+        prefix_count = {0:1}
+        sum_ = 0
+
+        for num in nums:
+            sum_ += num
+            
+            if sum_ - k in prefix_count:
+                subarray += 1
+                count += prefix_count[sum_ - k]
+            prefix_count[sum_] = prefix_count.get(sum_, 0) + 1
+
         return count
-        # prefix_sum = [0] * (len(nums) + 1)
-        # count = 0
-        # for idx in range(1, len(prefix_sum)):
-        #     prefix_sum[idx] = prefix_sum[idx-1] + nums[idx-1]
-
-        # # print(prefix_sum)
-
-        # for idx in range(len(nums)+1):
-        #     for end in range(idx+1, len(nums)+1):
-        #         if(prefix_sum[end] - prefix_sum[idx] == k):
-        #             count += 1
-
-        # return count    
