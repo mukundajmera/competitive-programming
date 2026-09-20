f6d56f89616668ecd2998ecc018f9690afd093d8
Mukund Ajmera <mukundajmera94@gmail.com>
2026-09-20 23:15:36 +0530

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: 28 ms (82.44%), Space: 21.8 MB (85.05%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,6 +1,5 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        subarray = 0
         count = 0
         prefix_count = {0:1}
         sum_ = 0
@@ -9,7 +8,6 @@ class Solution:
             sum_ += num
             
             if sum_ - k in prefix_count:
-                subarray += 1
                 count += prefix_count[sum_ - k]
             prefix_count[sum_] = prefix_count.get(sum_, 0) + 1
 
